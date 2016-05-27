7a3dbbd71558690137fb9b19811735368d92fadf
liugang <136308332@qq.com>
2016-05-27 11:41:36 +0800

    创建默认的Android工程

---
+++ app/src/main/java/com/liugang/day30_gitdemo1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.liugang.day30_gitdemo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private long lastTime;
+    @Override
+    public void onBackPressed() {
+
+        long ct = System.currentTimeMillis();
+        if(ct - lastTime>2000){
+            Toast.makeText(MainActivity.this, "再按返回键退出", Toast.LENGTH_SHORT).show();
+            lastTime = ct;
+        }else{
+            super.onBackPressed();
+
+        }
+
+    }
+}
